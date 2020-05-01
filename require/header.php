c68521dd7e54168589096ea26a16999c9da8328f
dilipsuthar1999 <61174714+dilipsuthar1999@users.noreply.github.com>
2020-05-02 00:58:05 +0530

    Create header.php

---
+++ require/header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img src="image/logo.png" class="navbar-brand" style="width:100px; height: 50px;">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php"><b>Home</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="movies.php"><b>Movies</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="tvshows.php"><b>TV Shows</b></a>
+      </li>
+    </ul>
+    <a class="nav-link" href="logout.php"><b>Logout</b></a>
+  </div>
+</nav>
