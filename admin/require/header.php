72e7aff1c199f8d77ea388b9b1ccd5f060824ec5
dilipsuthar1999 <61174714+dilipsuthar1999@users.noreply.github.com>
2020-05-02 01:01:48 +0530

    Create header.php

---
+++ admin/require/header.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img src="image/logo.png" class="navbar-brand" style="width:100px; height: 50px;">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#"><b>Home</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModal1"><b>Add Movie</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModal2"><b>Add TV Show</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModal3"><b>Add TV Show(Episode)</b></a>
+      </li>
+    </ul>
+    <a class="nav-link" href="logout.php"><b>Logout</b></a>
+  </div>
+</nav>
